0cfe3a0b1518872cddeb3c3cce1e50e0fecc5065
Marco Alonso <marcoalonsorota@hotmail.com>
2024-10-06 23:17:29 -0600

    Initial Commit

---
+++ TrackYou/TrackYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackYouApp.swift
+//  TrackYou
+//
+//  Created by Marco Alonso on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
